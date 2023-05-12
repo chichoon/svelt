0ecc589383c4d9be00d9685b4c5ec7ae4d8b67a9
chichoon <chichoon.choi@gmail.com>
2023-05-12 15:40:57 +0900

    chore: :bento: svg 추가

---
+++ src/assets/infoIcon.svelte
@@ -0,0 +1,11 @@
+<svg fill="#000000" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+	 width="800px" height="800px" viewBox="0 0 453.725 453.725"
+	 xml:space="preserve">
+<g>
+	<g>
+		<polygon points="248.374,421.928 288.675,150.442 145.771,150.442 145.771,182.241 192.257,182.241 151.956,453.725
+			295.345,453.725 295.345,421.928 		"/>
+		<circle cx="246.587" cy="61.367" r="61.367"/>
+	</g>
+</g>
+</svg>
