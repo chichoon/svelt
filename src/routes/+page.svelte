5a263193170efcf644a3b7ae02a2e130d3564c4c
chichoon <chichoon.choi@gmail.com>
2023-05-12 20:06:32 +0900

    chore: sveltekit으로 다시 레포지토리 초기화

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+  import GoHomeButton from "@/components/NavButtonSection/GoHomeButton.svelte";
+  import FileIcon from "@/components/NavButtonSection/FileIcon.svelte";
+</script>
+
+<main>
+  <GoHomeButton />
+  <FileIcon />
+</main>
+
+<style lang="scss">
+</style>
