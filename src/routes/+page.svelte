ee076f6e3404cdcaba4e6fee5143ab040168baa1
chichoon <chichoon.choi@gmail.com>
2023-05-15 23:30:39 +0900

    style: :lipstick: 화면에 이미지 배치

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,26 @@
   import GoHomeButton from "@/components/NavButtonSection/GoHomeButton.svelte";
   import FileIcon from "@/components/NavButtonSection/FileIcon.svelte";
   import NavButtonSection from "@/components/NavButtonSection/NavButtonSection.svelte";
+
+  import operatorImg from "@/assets/test.png";
+  import ReactIcon from "@/assets/ReactIcon.svelte";
 </script>
 
-<main>
+<main class="statPageBackgroundWrapper">
   <NavButtonSection />
+  <div class="groupIconWrapper">
+    <ReactIcon />
+  </div>
+  <img src={operatorImg} alt="operator" class="characterImage" />
 </main>
 
 <style lang="scss">
+  @use "@/styles/colors";
+  @import "./+page.scss";
+
+  .groupIconWrapper :global(svg) {
+    width: 220px;
+    height: 220px;
+    fill: colors.$GROUP_ICON;
+  }
 </style>
