52551b059169b607af372465e29aa3ec24d2efc6
chichoon <chichoon.choi@gmail.com>
2023-05-20 15:44:43 +0900

    style: :lipstick: 폰트 붙이기

---
+++ src/components/LeftStatusSection/NameArea/TrustStat.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import FastForwardIcon from "@/assets/FastForwardIcon.svelte";
+
+  export let trustPercentage: number;
+</script>
+
+<div class="trustWrapper">
+  <div class="trustTextWrapper">
+    <div class="trustLeftText">
+      <span class="trustText">신뢰도</span>
+      <FastForwardIcon />
+    </div>
+    <span class="trustText">{trustPercentage}</span>
+  </div>
+  <div
+    class="progressBar"
+    style={`background: linear-gradient(to right, #ffaf6d 0 ${trustPercentage}%, #d8d8d8 ${trustPercentage}% 100%)`}
+  />
+</div>
+
+<style lang="scss">
+  @use "@/styles/colors";
+  @import "./trustStat.scss";
+
+  .trustLeftText :global(svg) {
+    width: 10px;
+    height: 10px;
+    fill: colors.$BUTTON_ICON_DARK;
+    margin-left: 5px;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,19 @@
   import LeftStatusSection from "@/components/LeftStatusSection/LeftStatusSection.svelte";
 </script>
 
+<svelte:head
+  ><link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link
+    rel="preconnect"
+    href="https://fonts.gstatic.com"
+    crossorigin="use-credentials"
+  />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Nanum+Myeongjo:wght@700&family=Noto+Sans+KR&display=swap"
+    rel="stylesheet"
+  />
+</svelte:head>
+
 <main class="statPageBackgroundWrapper">
   <NavButtonSection />
   <div class="groupIconWrapper">
