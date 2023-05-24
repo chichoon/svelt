3079adbb5f2c6a4ae3a93625f0a1fa1c23825e22
chichoon <chichoon.choi@gmail.com>
2023-05-24 17:38:42 +0900

    feat: :lipstick: 아이콘 2개 추가

---
+++ src/components/RightStatusSection/EliteStats/EliteIcon.svelte
@@ -0,0 +1,39 @@
+<script>
+  export const indexes = [2, 3, 6, 9];
+</script>
+
+<div class="eliteIconWrapper">
+  {#each indexes as index}
+    <div
+      style={`top: ${
+        (index % 2 ? (index / 3) * 8 : (index / 2) * 8) + 10
+      }px; z-index: ${index % 2 ? 15 - index : 10 - index}`}
+      class={`eliteParallelogram ${index % 2 ? "odd" : ""}`}
+    />
+  {/each}
+</div>
+
+<style lang="scss">
+  @use "@/styles/colors";
+
+  .eliteIconWrapper {
+    width: 90px;
+    height: 90px;
+    position: relative;
+  }
+
+  .eliteParallelogram {
+    width: 50px;
+    height: 20px;
+    background-color: colors.$WHITE;
+    filter: drop-shadow(0 0 2px colors.$BOX_SHADOW_THICK);
+    transform: skewX(-45deg) scaleX(0.5) rotate(-45deg);
+    position: absolute;
+    left: 30px;
+
+    &.odd {
+      transform: skew(45deg) scaleX(0.5) rotate(45deg);
+      left: 10px;
+    }
+  }
+</style>
---
+++ src/components/RightStatusSection/EliteStats/PotentialIcon.svelte
@@ -0,0 +1,42 @@
+<script>
+  export const styleValues = [
+    { deg: 36, left: 5, top: 30 },
+    { deg: 72, left: 25, top: 17.5 },
+    { deg: 108, left: 0, top: 15 },
+    { deg: 144, left: 20, top: 30 },
+    { deg: 180, left: 15, top: 7.5 },
+  ];
+</script>
+
+<div class="potentialIconWrapper">
+  {#each styleValues as value}
+    <div
+      class="potentialParallelogramWrapper"
+      style={`transform: rotate(${value.deg}deg); top: ${value.top}px; left: ${value.left}px;`}
+    >
+      <div class="potentialParallelogram" />
+    </div>
+  {/each}
+</div>
+
+<style lang="scss">
+  @use "@/styles/colors";
+
+  .potentialIconWrapper {
+    width: 90px;
+    height: 90px;
+    position: relative;
+    filter: drop-shadow(0 3px 0px colors.$HIGHLIGHT_BLUE);
+  }
+
+  .potentialParallelogramWrapper {
+    position: absolute;
+  }
+
+  .potentialParallelogram {
+    width: 40px;
+    height: 20px;
+    background-color: colors.$WHITE;
+    transform: skewX(45deg) scaleX(0.5) rotate(45deg);
+  }
+</style>
