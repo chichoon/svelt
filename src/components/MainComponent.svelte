9fa8a8d0d41eae87f7a2942a6708323057cc6cff
chichoon <chichoon.choi@gmail.com>
2023-09-26 17:34:48 +0900

    feat: main component 추가 (모든 컴포넌트 모아보는..)

---
+++ src/components/MainComponent.svelte
@@ -0,0 +1,17 @@
+<script>
+  import NavButtonSection from "./NavButtonSection/NavButtonSection.svelte";
+    import GitHubIcon from '@/assets/GitHubIcon.svelte';
+    import operatorImg from "@/assets/images/operator.png";
+  import LeftStatusSection from "./LeftStatusSection/LeftStatusSection.svelte";
+  import RightStatusSection from "./RightStatusSection/RightStatusSection.svelte";
+</script>
+
+<main class="statPageBackgroundWrapper">
+    <NavButtonSection />
+    <div class="groupIconWrapper">
+        <GitHubIcon />
+    </div>
+    <img src={operatorImg} alt="operator" class="characterImage" />
+    <LeftStatusSection />
+    <RightStatusSection />
+</main>
